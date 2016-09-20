cf0b2a36812e81e89b6618dac6cfedd4a569878b
Yigal Duppen <yigal@publysher.nl>
2016-09-20 13:15:55 +0200

    TG-331 Docker-wait for the correct environment

--- web/afvalophaalgebieden/docker-wait.sh
+++ web/afvalophaalgebieden/docker-wait.sh
@@ -4,7 +4,7 @@ set -u
 set -e
 
 # wait for postgres
-while ! nc -z ${DATABASE_PORT_5432_TCP_ADDR} ${DATABASE_PORT_5432_TCP_PORT}
+while ! nc -z ${DB_HOST} ${DB_PORT_5432}
 do
 	echo "Waiting for postgres..."
 	sleep 0.1
