30a2ef3f3189a87ceee9bbc25f6ebc48f8408ba1
vegter <amsjurien@gmail.com>
2017-11-14 17:04:58 +0100

    Rename tijd_van in grofvuil database to tijd_vanaf Add simple database migration Test for database contents extended to both tables Swagger documentation added

--- web/afvalophaalgebieden/check_db.py
+++ web/afvalophaalgebieden/check_db.py
@@ -1,19 +1,28 @@
+from sqlalchemy import Table, MetaData
 from sqlalchemy.exc import ProgrammingError
 
-from app import models
+from app import models, db
 from run_import import run_all
 
 
 def check_db():
     try:
+        # Test if databases exist
         models.Huisvuil.query.count()
+        models.Grofvuil.query.count()
     except ProgrammingError:
         models.recreate_db()
         models.db.session.rollback()
         models.db.drop_all()
         models.db.create_all()
 
-    if not models.Huisvuil.query.count():
+    # Simple migration strategy. If more migrations occur please consider using flask-migrate
+    meta = MetaData()
+    messages = Table('grofvuil', meta, autoload=True, autoload_with=db.engine)
+    if not 'tijd_vanaf' in [c.name for c in messages.columns]:
+        models.db.create_all()
+
+    if not (models.Huisvuil.query.count() and models.Grofvuil.query.count()):
         run_all()
 
 
