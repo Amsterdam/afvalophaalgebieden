4b891a3246f0ca61c44997e351b372a90987c0f3
Eelke Jager <info@lytrix.com>
2019-07-23 22:20:46 +0200

    Fixed wrong order of import shapefiles step

--- afvalophaalgebieden/docker-import-db.sh
+++ afvalophaalgebieden/docker-import-db.sh
@@ -6,10 +6,10 @@ set -x   # log every command.
 
 source docker-wait.sh
 
-python check_db.py /data
-
 # download files
 python download_files_from_dcatd.py qji2W_HBpWUWyg /data
+
+python check_db.py /data
 
 # load data in database
 python run_import.py /data
