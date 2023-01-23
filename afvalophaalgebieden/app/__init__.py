8fe62f01ffcda6a627016c91ba964e94bb8e90df
LB <78799392+lbam@users.noreply.github.com>
2023-01-23 14:50:58 +0100

    Remove flask-swagger dep, no longer used

--- afvalophaalgebieden/app/__init__.py
+++ afvalophaalgebieden/app/__init__.py
@@ -3,7 +3,6 @@ import logging
 from flask import Flask, request, views, jsonify, abort, Response
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from flask_swagger import swagger
 from geoalchemy2.elements import WKTElement
 
 from pyproj import Proj, transform
