b3c8e88b6e49609584cb68753b9481be2f59e99b
LB <78799392+lbam@users.noreply.github.com>
2023-01-25 13:43:55 +0100

    Run check_db in app context (#55)

--- afvalophaalgebieden/check_db.py
+++ afvalophaalgebieden/check_db.py
@@ -1,29 +1,31 @@
 from sqlalchemy import Table, MetaData
 from sqlalchemy.exc import ProgrammingError
 
+import app
 from app import models, db
 from run_import import run_all
 
 
 def check_db():
-    try:
-        # Test if databases exist
-        models.Huisvuil.query.count()
-        models.Grofvuil.query.count()
-    except ProgrammingError:
-        models.recreate_db()
-        models.db.session.rollback()
-        models.db.drop_all()
-        models.db.create_all()
+    with app.application.app_context():
+        try:
+            # Test if databases exist
+            models.Huisvuil.query.count()
+            models.Grofvuil.query.count()
+        except ProgrammingError:
+            models.recreate_db()
+            models.db.session.rollback()
+            models.db.drop_all()
+            models.db.create_all()
 
-    # Simple migration strategy. If more migrations occur please consider using flask-migrate
-    meta = MetaData()
-    messages = Table('grofvuil', meta, autoload=True, autoload_with=db.engine)
-    if not 'tijd_vanaf' in [c.name for c in messages.columns]:
-        models.db.create_all()
+        # Simple migration strategy. If more migrations occur please consider using flask-migrate
+        meta = MetaData()
+        messages = Table('grofvuil', meta, autoload=True, autoload_with=db.engine)
+        if not 'tijd_vanaf' in [c.name for c in messages.columns]:
+            models.db.create_all()
 
-    if not (models.Huisvuil.query.count() and models.Grofvuil.query.count()):
-        run_all()
+        if not (models.Huisvuil.query.count() and models.Grofvuil.query.count()):
+            run_all()
 
 
 if __name__ == '__main__':
